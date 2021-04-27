727c1a244517528d054ae3a8d0022fd8f6c3effb
Micha <goette@math.tu-berlin.de>
2021-04-27 15:24:44 +0200

    update

--- code/BuildHamiltonian.cpp
+++ code/BuildHamiltonian.cpp
@@ -93,7 +93,7 @@ Tensor V22f(size_t i,size_t d){
 	comp[{0,getsizeV22(i,d)-1}] = 1;
 	comp[{d-i+1,getsizeV22(i,d)-1}] = 1;
 	comp[{getsizeV22(i,d)-1,getsizeV22(i,d)-1}] = 1;comp[{getsizeV22(i,d)-1,getsizeV22(i,d)-1}] = 1;
-	for(size_t j = 0; j < d-i;++j){
+	for(size_t j = 0; j < d-i-1;++j){
 		comp[{j+1,j}] = 1; comp[{j+1,j}] = -1;
 		comp[{d-i+2+j,d-i+j}] = 1; comp[{d-i+2+j,d-i+j}] = -1;
 	}
