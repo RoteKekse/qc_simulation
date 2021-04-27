e02e7b119c793e9f85521792902ab64bacef6b3a
Micha <goette@math.tu-berlin.de>
2021-04-27 14:50:08 +0200

    update

--- code/BuildHamiltonian.cpp
+++ code/BuildHamiltonian.cpp
@@ -8,38 +8,106 @@ using xerus::misc::operator<<;
 value_t returnTValue(size_t p, size_t q);
 value_t returnVValue(size_t i, size_t k, size_t j, size_t l);
 TTOperator buildHamil(Tensor &T, Tensor &V);
+Tensor V11f(size_t i,size_t d);
+Tensor V22f(size_t i,size_t d);
+
 
 int main(int argc, char* argv[]) {
-	/*
-	 * !!!!! Change Here !!!!
-	 */
-	const auto geom = argv[1];
-	// Set basis functions
-	const auto basisname = argv[2];
 
-	auto T = xerus::Tensor();
-	auto V = xerus::Tensor();
-
-	std::string name;
-	name = "data/"+static_cast<std::string>(geom)+"_"+static_cast<std::string>(basisname)+"_T.tensor";
-	read_from_disc(name,T);
-	name = "data/"+static_cast<std::string>(geom)+"_"+static_cast<std::string>(basisname)+"_V.tensor";
-	read_from_disc(name,V);
+	XERUS_LOG(info,"\n" << V11f(0,8));
+	XERUS_LOG(info,"\n" << V22f(3,8));
 
 
-
-	H = buildHamil(T,V);
-	name = "data/"+static_cast<std::string>(geom)+"_"+static_cast<std::string>(basisname)+"_H.ttoperator";
-	write_to_disc(name,V);
-
-	return 0;
+//	const auto geom = argv[1];
+//	// Set basis functions
+//	const auto basisname = argv[2];
+//
+//	auto T = xerus::Tensor();
+//	auto V = xerus::Tensor();
+//
+//	std::string name;
+//	name = "data/"+static_cast<std::string>(geom)+"_"+static_cast<std::string>(basisname)+"_T.tensor";
+//	read_from_disc(name,T);
+//	name = "data/"+static_cast<std::string>(geom)+"_"+static_cast<std::string>(basisname)+"_V.tensor";
+//	read_from_disc(name,V);
+//
+//
+//
+//	H = buildHamil(T,V);
+//	name = "data/"+static_cast<std::string>(geom)+"_"+static_cast<std::string>(basisname)+"_H.ttoperator";
+//	write_to_disc(name,V);
+//
+//	return 0;
+//}
+//TTOperator buildHamil(Tensor &T, Tensor &V){
+//	size d = 2*T.dimensions[0];
+//	TTOperator H(std::vector<size_t>(d,2));
+//
+//	return H;
 }
-TTOperator buildHamil(Tensor &T, Tensor &V){
-	size d = 2*T.dimensions[0];
-	TTOperator H(std::vector<size_t>(d,2));
 
-	return H;
+Tensor V11f(size_t i,size_t d){
+	bool reverse = i < d/2 ? false : true;
+	if reverse
+		i = d-1-i;
+	Tensor comp({i == 0 ? 1 : getsizeV11(i-1),2,2,getsizeV11(i)});
+	comp[0,0,0,0] = 1; comp[0,1,1,0] = 1;
+	comp[0,1,0,i+1] = 1;
+	comp[0,0,1,2*(i+1)] = 1;
+	comp[0,1,1,2*(i+1)+1+i*i] = 1;
+	for(size_t j = 0;j<i;++j){
+		comp[j+1,0,0,j+1] = 1; comp[j+1,1,1,j+1] = -1;
+		comp[j+i+1,0,0,j+i+1] = 1; comp[j+i+1,1,1,j+i+1] = -1;
+		comp[j+1,0,1,j+1+i*i+2*(i+1)+1] = 1;
+		comp[j+1,1,0,j+1+i*i+ 4*(i+1)+i*(i-1)/2-1] = 1;
+		comp[j+i+1,1,0,j+1+i*i+ 3*(i+1)] = 1;
+		comp[j+i+1,0,1,j+1+i*i+ 5*(i+1)+i*(i-1)-2] = 1;
+	}
+	for(size_t j = 0; j < i*i;++j){
+		comp[j+1+2*i,0,0,j+1+2*(i+1)] = 1;comp[j+1+2*i,1,1,j+1+2*(i+1)] = 1;
+	}
+	for(size_t j = 0;  j <i*(i-1)/2; ++j){
+		comp[j+1+2*i+i*i,0,0,j+i*i+ 4*(i+1)] = 1; comp[j+1+2*i+i*i,1,1,j+i*i+ 4*(i+1)] = -1;
+		comp[j+1+2*i+i*i+i*(i-1)/2,0,0,j+1+i*i+ 5*(i+1)+i*(i-1)/2-2] = 1; comp[j+1+2*i+i*i+i*(i-1)/2,1,1,j+1+i*i+ 5*(i+1)+i*(i-1)/2-2] = -1;
+	}
+
+	if reverse{
+		Index i1,j1;
+		comp(i1,j1) = comp(j1,i1)
+		return comp;
+	}
+	return comp;
 }
+
+Tensor V22f(size_t i,size_t d){
+	bool reverse = i < d/2 ? false : true;
+	if reverse
+		i = d-1-i;
+	Tensor comp({i == 0 ? 1 : getsizeV22(i-1),2,2,getsizeV22(i)});
+	comp[0,0,1,getsizeV22(i)-1] = 1;
+	comp[d-i+1,1,0,getsizeV22(i)-1] = 1;
+	comp[getsizeV22(i)-1,0,0,getsizeV22(i)-1] = 1;comp[getsizeV22(i)-1,1,1,getsizeV22(i)-1] = 1
+	for(size_t j = 0; j < d-i;++j){
+		comp[j+1,0,0,j] = 1; comp[j+2,1,1,j+1] = -1;
+		comp[d-i+2+j,0,0,d-i+j] = 1; comp[d-i+2+j,1,1,d-i+j] = -1;
+	}
+	if reverse{
+		Index i1,j1;
+		comp(i1,j1) = comp(j1,i1)
+		return comp;
+	}
+	return comp;
+}
+
+
+size_t getsizeV11(size_t i){
+    return 1+2*(i+1)+(i+1)^2+(i+1)*(i);
+}
+
+size_t getsizeV22(size_t i,size_t d){
+    return 1+2*(d-i-1);
+}
+
 
 
 value_t returnTValue(Tensor T, size_t i, size_t j)
