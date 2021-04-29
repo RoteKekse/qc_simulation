c62169e83e5b0db8b648dce1624688cfd9730b89
Micha <goette@math.tu-berlin.de>
2021-04-29 15:34:24 +0200

    update

--- code/BuildHamiltonian.cpp
+++ code/BuildHamiltonian.cpp
@@ -260,8 +260,8 @@ Tensor MVf(Tensor T, Tensor V){
     size_t n = getsizeV11(d/2-1)+getsizeV22(d/2-1,d);
     Tensor MV({n,n});
 
-    MV[n-1,0] = 1;
-    MV[0,n-1] = 1;
+    MV[{n-1,0}] = 1;
+    MV[{0,n-1}] = 1;
     for (size_t i=0; i< d/2;++i){
         MV[{1+i,n-i-2}] = 1;
         MV[{1+d/2+i,n-2-i-d/2}] = 2;//1;
