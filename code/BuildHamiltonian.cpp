0bc51796d133dd6445ac3bffbfd2182faf716556
Micha <goette@math.tu-berlin.de>
2021-04-30 15:21:48 +0200

    update

--- code/BuildHamiltonian.cpp
+++ code/BuildHamiltonian.cpp
@@ -52,7 +52,7 @@ int main(int argc, char* argv[]) {
 					std::vector<size_t> idx(4*nob,0);
 					idx[i] = 1; idx[j] = 1; idx[k+2*nob] = 1;idx[l+2*nob] = 1;
 					if (std::abs(H[idx]-H_bench[idx]) > 1e-10){
-						value_t correct = getV(i,j,k,l);
+						value_t correct = getV(V,i,j,k,l);
 
 						XERUS_LOG(info,i << " " << j << " " <<k << " " <<l << " " << H[idx]<< " " <<H_bench[idx] << " " << correct);
 					}
