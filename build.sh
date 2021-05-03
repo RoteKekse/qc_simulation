8b9cacb0894a6c857d038f647a720b67999bf354
Micha <goette@math.tu-berlin.de>
2021-05-03 15:18:59 +0200

    update

--- build.sh
+++ build.sh
@@ -6,7 +6,8 @@ g++ -std=c++14 -g -o  build/get_2elec_int.out code/Create2ParticleOperator.cc  -
 g++ -std=c++14 -g -o  build/basistransform.out code/BasisTransform.cpp  -lint2 -lpthread -lxerus -lxerus_misc
 g++ -std=c++14 -g -o  build/builhamil.out code/BuildHamiltonian.cpp   -lxerus -lxerus_misc
 
-g++ -g -std=c++14 -fext-numeric-literals -DARPACK_LIBRARIES -o build/buildprecon.out code/BuildPrecon.cpp   -lxerus -lxerus_misc -lboost_regex -fopenmp
+g++ -g -std=c++14  -o build/buildprecon.out code/BuildPrecon.cpp   -lxerus -lxerus_misc 
+
 
 
 
