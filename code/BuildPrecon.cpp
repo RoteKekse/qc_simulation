62cefa6e3d889e4e6bfd7b555c2010029b003bc1
Micha <goette@math.tu-berlin.de>
2021-05-03 16:11:51 +0200

    update

--- code/BuildPrecon.cpp
+++ code/BuildPrecon.cpp
@@ -222,7 +222,7 @@ TTOperator build_Fock_op_inv2(std::vector<value_t> coeffs, const size_t k, value
 			aa[{0,1,1,0}] = fac*fac2;
 			tmp.set_component(i,aa);
 		}
-		XERUS_LOG(info,j << " " << tmp.frob_norm() << " " << fac);
+		XERUS_LOG(info,j << " " << tmp.frob_norm() << " " << fac << " " << beta);
 		result += tmp;
 	}
 	return result;
