2c6865d25c605ba3c44cd7a76e412f0d68224337
Micha <goette@math.tu-berlin.de>
2021-05-04 14:20:39 +0200

    update

--- code/BuildPrecon.cpp
+++ code/BuildPrecon.cpp
@@ -115,10 +115,10 @@ int main(int argc, char* argv[]) {
 	TTOperator test, Fock = build_Fock_op(HFev);
 	Fock += shift*TTOperator::identity(std::vector<size_t>(4*nob,2));
 
-	test(ii^(2*nob),jj^(2*nob)) = Fock(ii^(2*nob),kk^(2*nob)) * Fock_inv(kk^(2*nob),jj^(2*nob));
-	test += TTOperator::identity(std::vector<size_t>(4*nob,2));
-	test.move_core(0);
-	XERUS_LOG(info,"Approximation error = " <<std::setprecision(12) <<test.frob_norm());
+//	test(ii^(2*nob),jj^(2*nob)) = Fock(ii^(2*nob),kk^(2*nob)) * Fock_inv(kk^(2*nob),jj^(2*nob));
+//	test += TTOperator::identity(std::vector<size_t>(4*nob,2));
+//	test.move_core(0);
+//	XERUS_LOG(info,"Approximation error = " <<std::setprecision(12) <<test.frob_norm());
 
 
 	test(ii^(2*nob),jj^(2*nob)) = Fock(ii^(2*nob),kk^(2*nob)) * Fock_inv2(kk^(2*nob),jj^(2*nob));
