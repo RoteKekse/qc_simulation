b028637cc336de85669620449b3580fa46f4f4a2
Micha <goette@math.tu-berlin.de>
2021-05-04 14:22:08 +0200

    update

--- code/BuildPrecon.cpp
+++ code/BuildPrecon.cpp
@@ -103,15 +103,6 @@ int main(int argc, char* argv[]) {
 	write_to_disc(name,Fock_inv);
 	XERUS_LOG(info,Fock_inv.ranks());
 
-	xerus::Index ii,jj,kk,ll,i1,i2,i3,i4,j1,j2,j3,j4,k1,k2,k3,k4;
-	TTOperator test, Fock = build_Fock_op(HFev);
-	Fock += shift*TTOperator::identity(std::vector<size_t>(4*nob,2));
-
-	test(ii^(2*nob),jj^(2*nob)) = Fock(ii^(2*nob),kk^(2*nob)) * Fock_inv(kk^(2*nob),jj^(2*nob));
-	test += TTOperator::identity(std::vector<size_t>(4*nob,2));
-	test.move_core(0);
-	XERUS_LOG(info,"Approximation error = " <<std::setprecision(12) <<test.frob_norm());
-
 
 	TTOperator Fock_inv2 = build_Fock_op_inv2(HFev, k, shift_vec);
 	name = "data/"+static_cast<std::string>(geom)+"_"+static_cast<std::string>(basisname)+"_Finv2.ttoperator";
@@ -120,7 +111,14 @@ int main(int argc, char* argv[]) {
 	XERUS_LOG(info,Fock_inv2.ranks());
 
 
+	xerus::Index ii,jj,kk,ll,i1,i2,i3,i4,j1,j2,j3,j4,k1,k2,k3,k4;
+	TTOperator test, Fock = build_Fock_op(HFev);
+	Fock += shift*TTOperator::identity(std::vector<size_t>(4*nob,2));
 
+	test(ii^(2*nob),jj^(2*nob)) = Fock(ii^(2*nob),kk^(2*nob)) * Fock_inv(kk^(2*nob),jj^(2*nob));
+	test += TTOperator::identity(std::vector<size_t>(4*nob,2));
+	test.move_core(0);
+	XERUS_LOG(info,"Approximation error = " <<std::setprecision(12) <<test.frob_norm());
 
 
 	test(ii^(2*nob),jj^(2*nob)) = Fock(ii^(2*nob),kk^(2*nob)) * Fock_inv2(kk^(2*nob),jj^(2*nob));
