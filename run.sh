464852492f7414ae5f9e4a80b55edd6a6e5300a2
Micha <goette@math.tu-berlin.de>
2021-05-04 22:10:18 +0200

    update

--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@ FILE=n2
 BASIS=cc-pvtz
 SHIFT=110
 K=700
+H=0.05
 
 build/hf.out $PATH $FILE $BASIS
 build/get_1elec_int.out $PATH $FILE $BASIS
@@ -11,4 +12,4 @@ build/basistransform.out $FILE $BASIS
 
 build/buildhamil.out $FILE $BASIS
 build/buildhamildiag.out $FILE $BASIS
-build/buildprecon.out $FILE $BASIS $SHIFT $K
+build/buildprecon.out $FILE $BASIS $SHIFT $K $H
