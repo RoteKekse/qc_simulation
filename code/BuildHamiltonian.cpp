5b6655277d03fefc500f4353ee1b914d69a409be
Micha <goette@math.tu-berlin.de>
2021-05-04 18:27:59 +0200

    update

--- code/BuildHamiltonian.cpp
+++ code/BuildHamiltonian.cpp
@@ -29,15 +29,14 @@ int main(int argc, char* argv[]) {
 
 	Tensor T ,V;
 	TTOperator H_bench;
-	//std::string name = "data/"+static_cast<std::string>(geom)+"_"+static_cast<std::string>(basisname)+"_T.tensor";
-	std::string name = "data/T_H2O_48_bench_single.tensor";
+	std::string name = "data/"+static_cast<std::string>(geom)+"_"+static_cast<std::string>(basisname)+"_T.tensor";
 	read_from_disc(name, T);
-	name = "data/V_H2O_48_bench_single.tensor";
+	name = "data/"+static_cast<std::string>(geom)+"_"+static_cast<std::string>(basisname)+"_V.tensor";
 	read_from_disc(name, V);
 
 	auto H = BuildHamil(T,V);
-	size_t d = H.order()/2;
-	std::vector<size_t> hf = {0,1,2,3,22,23,30,31};
+	size_t d = H.order();
+	std::vector<size_t> hf = {0,1,2,3,4,5,6,7,8,9,10,11,12,13};
 	std::vector<size_t> idx(2*d,0);
 	for (auto  i : hf){
 		idx[i] = 1;
