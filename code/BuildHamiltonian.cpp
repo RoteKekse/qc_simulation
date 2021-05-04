b9f15a48a28ff30ffaf005c2e08c968740d2ee35
Micha <goette@math.tu-berlin.de>
2021-05-04 18:20:36 +0200

    update

--- code/BuildHamiltonian.cpp
+++ code/BuildHamiltonian.cpp
@@ -35,6 +35,14 @@ int main(int argc, char* argv[]) {
 	read_from_disc(name, V);
 
 	auto H = BuildHamil(T,V);
+	size_t d = H.order();
+	sdt::vector<size_t> hf = {0,1,2,3,4,5,6,7,8,9,10,11,12,13};
+	std::vector<size_t> idx(2*d,0);
+	for (auto  i : hf){
+		idx[i] = 1;
+		idx[i+d] = 1;
+	}
+	XERUS_LOG(info, H[idx]);
 
 	name = "data/"+static_cast<std::string>(geom)+"_"+static_cast<std::string>(basisname)+"_H.ttoperator";
 	write_to_disc(name, H);
