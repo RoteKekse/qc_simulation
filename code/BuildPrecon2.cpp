568993fe33ba98ab8c7cc8494a02c5ba7e90b69d
Micha <goette@math.tu-berlin.de>
2021-05-05 14:17:53 +0200

    update

--- code/BuildPrecon2.cpp
+++ code/BuildPrecon2.cpp
@@ -219,7 +219,7 @@ int main(int argc, char* argv[]) {
 	name = "data/"+static_cast<std::string>(geom)+"_"+static_cast<std::string>(basisname)+"_Finv3.ttoperator";
 	auto xrandTTO = makeTTO(xrand, d)
 	write_to_disc(name,xrandTTO);
-	load_from_disc(name,xrandTTO);
+	read_from_disc(name,xrandTTO);
 
 	test(ii^d) = D(ii^d,jj^d) * xrand(jj^d);
 	test -=b;
