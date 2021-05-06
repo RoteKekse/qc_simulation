3f58d9c4cc0a9c60329e0bc3c8f7980753ceb7a7
Micha <goette@math.tu-berlin.de>
2021-05-06 16:29:34 +0200

    update

--- code/BuildPrecon2.cpp
+++ code/BuildPrecon2.cpp
@@ -243,7 +243,7 @@ int main(int argc, char* argv[]) {
 	phi =makeUnitVector({0,1,2,3,4,5,6,7,8,9,10,11,12,13},d);
 	test1() =  phi(ii^(d))*F(ii^(d),jj^(d)) * phi(jj^(d));
 	XERUS_LOG(info,"Fock = " <<test1[0]);
-	test2() =  phi(ii^(d))*makeTTO(xrand, d)(ii^(d),jj^(d)) * phi(jj^(d));
+	test2() =  phi(ii^(d))*xrandTTO(ii^(d),jj^(d)) * phi(jj^(d));
 	XERUS_LOG(info,"Fock inv= " <<test2[0]);
 	XERUS_LOG(info,"prod= " <<test1[0]*test2[0]);
 
@@ -256,10 +256,11 @@ int main(int argc, char* argv[]) {
 	XERUS_LOG(info, d);
 	XERUS_LOG(info,"Approximation error = " <<std::setprecision(12) <<test.frob_norm());
 
+	xrandTTO = makeTTO(xrand, d);
 	phi =makeUnitVector({0,1,2,3,4,5,6,7,8,9,10,11,12,13},d);
 	test1() =  phi(ii^(d))*F(ii^(d),jj^(d)) * phi(jj^(d));
 	XERUS_LOG(info,"Fock = " <<test1[0]);
-	test2() =  phi(ii^(d))*makeTTO(xrand, d)(ii^(d),jj^(d)) * phi(jj^(d));
+	test2() =  phi(ii^(d))*xrandTTO(ii^(d),jj^(d)) * phi(jj^(d));
 	XERUS_LOG(info,"Fock inv= " <<test2[0]);
 	XERUS_LOG(info,"prod= " <<test1[0]*test2[0]);
 
@@ -272,10 +273,11 @@ int main(int argc, char* argv[]) {
 	XERUS_LOG(info, d);
 	XERUS_LOG(info,"Approximation error = " <<std::setprecision(12) <<test.frob_norm());
 
+	xrandTTO = makeTTO(xrand, d);
 	phi =makeUnitVector({0,1,2,3,4,5,6,7,8,9,10,11,12,13},d);
 	test1() =  phi(ii^(d))*F(ii^(d),jj^(d)) * phi(jj^(d));
 	XERUS_LOG(info,"Fock = " <<test1[0]);
-	test2() =  phi(ii^(d))*makeTTO(xrand, d)(ii^(d),jj^(d)) * phi(jj^(d));
+	test2() =  phi(ii^(d))*xrandTTO(ii^(d),jj^(d)) * phi(jj^(d));
 	XERUS_LOG(info,"Fock inv= " <<test2[0]);
 	XERUS_LOG(info,"prod= " <<test1[0]*test2[0]);
 
