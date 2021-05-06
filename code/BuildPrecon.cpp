b900cec18a0122d385db96995b27df803a5efe9f
Micha <goette@math.tu-berlin.de>
2021-05-06 15:48:17 +0200

    update

--- code/BuildPrecon.cpp
+++ code/BuildPrecon.cpp
@@ -149,7 +149,7 @@ int main(int argc, char* argv[]) {
 	XERUS_LOG(info,"Approximation error = " <<std::setprecision(12) <<test.frob_norm());
 
 	Tensor test1,test2;
-	auto phi =makeUnitVector({0,1,2,3,4,5,6,7,8,9,10,11,12,13},2*nob)
+	auto phi =makeUnitVector({0,1,2,3,4,5,6,7,8,9,10,11,12,13},2*nob);
 	test1() =  phi(jj^(2*nob))*Fock(ii^(2*nob),jj^(2*nob)) * phi(jj^(2*nob));
 	XERUS_LOG(info,"Fock = " <<test1[0]);
 	test2() =  phi(jj^(2*nob))*Fock_inv2(ii^(2*nob),jj^(2*nob)) * phi(jj^(2*nob));
