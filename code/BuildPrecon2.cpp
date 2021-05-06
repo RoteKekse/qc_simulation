e97fb181dafc67d82effc6a4fc834fb5e5aeaf37
Micha <goette@math.tu-berlin.de>
2021-05-06 16:17:05 +0200

    update

--- code/BuildPrecon2.cpp
+++ code/BuildPrecon2.cpp
@@ -241,6 +241,12 @@ int main(int argc, char* argv[]) {
 	XERUS_LOG(info, d);
 	XERUS_LOG(info,"Approximation error = " <<std::setprecision(12) <<test.frob_norm());
 
+	phi =makeUnitVector({0,1,2,3,4,5,6,7,8,9,10,11,12,13},d);
+	test1() =  phi(ii^(d))*F(ii^(d),jj^(d)) * phi(jj^(d));
+	XERUS_LOG(info,"Fock = " <<test1[0]);
+	test2() =  phi(ii^(d))*xrandTTO(ii^(d),jj^(d)) * phi(jj^(d));
+	XERUS_LOG(info,"Fock inv= " <<test2[0]);
+	XERUS_LOG(info,"prod= " <<test1[0]*test2[0]);
 
 
 
