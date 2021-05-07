e6cd25084889238c03d87df708545f988c166ece
Micha <goette@math.tu-berlin.de>
2021-05-07 12:11:48 +0200

    update

--- code/BuildPrecon2.cpp
+++ code/BuildPrecon2.cpp
@@ -253,18 +253,18 @@ int main(int argc, char* argv[]) {
 	XERUS_LOG(info,"Approximation error = " <<std::setprecision(12) <<test.frob_norm());
 
 	XERUS_LOG(info, "Test Inv 1");
-	printError(Fock, Fock_inv, {0,1,2,3,4,5,6,7,8,9,10,11,12,13}, d/2);
-	printError(Fock, Fock_inv, {22,45,33,66,77,88,99,73,42,32,12,21,63,2},  d/2);
-	printError(Fock, Fock_inv, {0,1,2,3,4,5,6,7,8,9,10,11,12,17},  d/2);
-	printError(Fock, Fock_inv, {24,25,26,27,28,29,30,31,32,33,34,35,36,37},  d/2);
-	printError(Fock, Fock_inv, {90,91,92,93,94,95,96,97,98,99,100,102,103,104},  d/2);
+	printError(F, F1, {0,1,2,3,4,5,6,7,8,9,10,11,12,13}, d/2);
+	printError(F, F1, {22,45,33,66,77,88,99,73,42,32,12,21,63,2},  d/2);
+	printError(F, F1, {0,1,2,3,4,5,6,7,8,9,10,11,12,17},  d/2);
+	printError(F, F1, {24,25,26,27,28,29,30,31,32,33,34,35,36,37},  d/2);
+	printError(F, F1, {90,91,92,93,94,95,96,97,98,99,100,102,103,104},  d/2);
 
 	XERUS_LOG(info, "Test Inv rank 1 ");
-	printError(Fock, xrandTTO, {0,1,2,3,4,5,6,7,8,9,10,11,12,13},  d/2);
-	printError(Fock, xrandTTO, {22,45,33,66,77,88,99,73,42,32,12,21,63,2},  d/2);
-	printError(Fock, xrandTTO, {0,1,2,3,4,5,6,7,8,9,10,11,12,17},  d/2);
-	printError(Fock, xrandTTO, {24,25,26,27,28,29,30,31,32,33,34,35,36,37},  d/2);
-	printError(Fock, xrandTTO, {90,91,92,93,94,95,96,97,98,99,100,102,103,104},  d/2);
+	printError(F, xrandTTO, {0,1,2,3,4,5,6,7,8,9,10,11,12,13},  d/2);
+	printError(F, xrandTTO, {22,45,33,66,77,88,99,73,42,32,12,21,63,2},  d/2);
+	printError(F, xrandTTO, {0,1,2,3,4,5,6,7,8,9,10,11,12,17},  d/2);
+	printError(F, xrandTTO, {24,25,26,27,28,29,30,31,32,33,34,35,36,37},  d/2);
+	printError(F, xrandTTO, {90,91,92,93,94,95,96,97,98,99,100,102,103,104},  d/2);
 
 
 
