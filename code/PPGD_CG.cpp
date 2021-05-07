818896888025c2ddffd71afc23150858d18aff8d
Micha <goette@math.tu-berlin.de>
2021-05-07 12:14:43 +0200

    update

--- code/PPGD_CG.cpp
+++ code/PPGD_CG.cpp
@@ -48,6 +48,7 @@
 		TTOperator id=TTOperator::identity(std::vector<size_t>(4*nob,2)),Finv;
 
 		name = "data/"+static_cast<std::string>(geom)+"_"+static_cast<std::string>(basisname)+"_Finv_" +std::to_string(finvrank)+".ttoperator";
+		name = "data/"+static_cast<std::string>(geom)+"_"+static_cast<std::string>(basisname)+"_Finv.ttoperator";
 		read_from_disc(name,Finv);
 		XERUS_LOG(info,Finv.ranks());
 		//Finv = id;
