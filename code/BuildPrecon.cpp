37cf7f0c74ff5679b920d66107aad2fcb2f8f100
Micha <goette@math.tu-berlin.de>
2021-05-10 15:43:17 +0200

    update

--- code/BuildPrecon.cpp
+++ code/BuildPrecon.cpp
@@ -230,20 +230,23 @@ TTOperator build_Fock_op_inv(std::vector<value_t> coeffs, std::vector<value_t> s
 			aa[{0,1,1,0}] =  std::exp(-bv*coeff2) ;
 			tmp.set_component(i,aa);
 		}
+		for (size_t i = 0; i < dim; ++i){
+				coeff1 = shift_vec[i];
+				coeff2 = coeffs[i]+shift_vec[i];
+				auto aa = xerus::Tensor({1,2,2,1});
+				aa[{0,0,0,0}] =  std::exp(-b_v1*coeff1)  ;
+				aa[{0,1,1,0}] =  std::exp(-b_v1*coeff2) ;
+				result1.set_component(i,aa);
+		}
+		result1 *= a_v[0]/a;
 
 		result += av*tmp;
+		XERUS_LOG(info, (result-result1).frob_norm());
+
 		XERUS_LOG(info, result.frob_norm());
 	}
 
-	for (size_t i = 0; i < dim; ++i){
-		coeff1 = shift_vec[i];
-		coeff2 = coeffs[i]+shift_vec[i];
-		auto aa = xerus::Tensor({1,2,2,1});
-		aa[{0,0,0,0}] =  std::exp(-b_v1*coeff1)  ;
-		aa[{0,1,1,0}] =  std::exp(-b_v1*coeff2) ;
-		result1.set_component(i,aa);
-	}
-	result1 *= a_v[0]/a;
+
 
 	for (size_t i = 0; i < dim; ++i){
 		coeff1 = shift_vec[i];
@@ -270,7 +273,6 @@ TTOperator build_Fock_op_inv(std::vector<value_t> coeffs, std::vector<value_t> s
 	result1 += a_v[2]/a*tmp1;
 	XERUS_LOG(info, result1.frob_norm());
 
-	XERUS_LOG(info, (result-result1).frob_norm());
 		//result.round(0.0);
 		//XERUS_LOG(info,"j = " << j << " coeff2 " << coeff2 << " norm " << tmp.frob_norm()<< std::endl << result.ranks());
 
