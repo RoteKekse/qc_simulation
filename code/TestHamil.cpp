d75b7e51de5f7aa3834db50be7a0463e35b5f004
Micha <goette@math.tu-berlin.de>
2021-05-15 21:18:13 +0200

    update

--- code/TestHamil.cpp
+++ code/TestHamil.cpp
@@ -35,6 +35,23 @@ int main(){
 	XERUS_LOG(info,contract_TT(H1,phi,phi)+nuc[0]);
 
 	XERUS_LOG(info,contract_TT(H2,phi,phi)+nuc[0]);
+	size_t d = 48;
+	for (size_t i = 0; i < d; i++){
+		for (size_t j = 0; j < d; j++){
+			for (size_t k = 0; k < d; k++){
+				for (size_t l = 0; l < d; l++){
+					std::vector<size_t> idx(2*d,0):
+					idx[i,j,d+k,d+l] = 1;
+					value_t val1 = H1[idx];
+					value_t val2 = H2[idx];
+					if (val1 != val2){
+						XERUS_LOG(info,idx);
+					}
+				}
+			}
+		}
+	}
+
 
 
 
