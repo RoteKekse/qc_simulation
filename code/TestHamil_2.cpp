5c01decd3d2bbdb736869a481ff11c87b5b39879
Micha <goette@math.tu-berlin.de>
2021-05-16 08:50:54 +0200

    update

--- code/TestHamil_2.cpp
+++ code/TestHamil_2.cpp
@@ -496,7 +496,7 @@ int main(){
 					value_t val2 = H2[idx];
 					if (std::abs(val1-val2) > 1e-9){
 						XERUS_LOG(info,i << " " << j<< " " << k<< " " << l );
-						val = V2[{i,j,k,l}]+V2[{j,i,l,k}]V2[{j,i,k,l}]-V2[{i,j,l,k}];
+						val = V2[{i,j,k,l}]+V2[{j,i,l,k}]-V2[{j,i,k,l}]-V2[{i,j,l,k}];
 						XERUS_LOG(info,val1 << " " << val2 <<" "  << std::abs(val1-val2)<< " " << getV(V1,i, j, k, l)<< " " << val   <<  "\n" );
 						count2++;
 					}
