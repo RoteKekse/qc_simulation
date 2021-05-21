26ee0e5eaf00a1464611507766ea93a741605cf2
Micha <goette@math.tu-berlin.de>
2021-05-21 09:11:54 +0200

    update

--- code/TestContract.cpp
+++ code/TestContract.cpp
@@ -18,49 +18,49 @@ int main(){
 	auto c2 = H.get_component(61);
 	auto dim1 = c1.dimensions;
 	auto dim2 = c2.dimensions;
-	auto ones = Tensor::ones({dim1[2]});
+	auto ones = Tensor::ones({dim1[0]});
 	XERUS_LOG(info,c1.dimensions);
 	XERUS_LOG(info,c2.dimensions);
 
 	clock_t begin_time = clock();
-	tmp(ii^2,kk,ll,mm^2) = c1(ii^2,jj,kk)*c2(ll,jj,mm^2);
+	tmp(ii,kk,ll,mm^2) = ones(nn)* c1(nn,ii,jj,kk)*c2(ll,jj,mm^2);
 	XERUS_LOG(info,  (value_t) (clock() - begin_time) / CLOCKS_PER_SEC);
 
 	c1.use_dense_representation();
 	c2.use_dense_representation();
 	begin_time = clock();
-	tmp(ii^2,kk,ll,mm^2) = c1(ii^2,jj,kk)*c2(ll,jj,mm^2);
+	tmp(ii,kk,ll,mm^2) = ones(nn)* c1(nn,ii,jj,kk)*c2(ll,jj,mm^2);
 	XERUS_LOG(info,  (value_t) (clock() - begin_time) / CLOCKS_PER_SEC);
 
 	c1 = Tensor::random(dim1);
 	c1 = Tensor::random(dim2);
 	begin_time = clock();
-	tmp(ii^2,kk,ll,mm^2) = c1(ii^2,jj,kk)*c2(ll,jj,mm^2);
+	tmp(ii,kk,ll,mm^2) = ones(nn)* c1(nn,ii,jj,kk)*c2(ll,jj,mm^2);
 	XERUS_LOG(info,  (value_t) (clock() - begin_time) / CLOCKS_PER_SEC);
 
 	c1 = H.get_component(55);
 	c2 = H.get_component(56);
-	 ones = Tensor::ones({dim1[2]});
+	 ones = Tensor::ones({dim1[0]});
 	dim1 = c1.dimensions;
 	dim2 = c2.dimensions;
 	XERUS_LOG(info,c1.dimensions);
 	XERUS_LOG(info,c2.dimensions);
 
 	begin_time = clock();
-	tmp(ii^2,kk,ll,mm^2) = c1(ii^2,jj,kk)*c2(ll,jj,mm^2);
-		XERUS_LOG(info,  (value_t) (clock() - begin_time) / CLOCKS_PER_SEC);
+	tmp(ii,kk,ll,mm^2) = ones(nn)* c1(nn,ii,jj,kk)*c2(ll,jj,mm^2);
+	XERUS_LOG(info,  (value_t) (clock() - begin_time) / CLOCKS_PER_SEC);
 
-		c1.use_dense_representation();
-		c2.use_dense_representation();
-		begin_time = clock();
-		tmp(ii^2,kk,ll,mm^2) = c1(ii^2,jj,kk)*c2(ll,jj,mm^2);
-		XERUS_LOG(info,  (value_t) (clock() - begin_time) / CLOCKS_PER_SEC);
+	c1.use_dense_representation();
+	c2.use_dense_representation();
+	begin_time = clock();
+	tmp(ii,kk,ll,mm^2) = ones(nn)* c1(nn,ii,jj,kk)*c2(ll,jj,mm^2);
+	XERUS_LOG(info,  (value_t) (clock() - begin_time) / CLOCKS_PER_SEC);
 
-		c1 = Tensor::random(dim1);
-		c1 = Tensor::random(dim2);
-		begin_time = clock();
-		tmp(ii^2,kk,ll,mm^2) = c1(ii^2,jj,kk)*c2(ll,jj,mm^2);
-		XERUS_LOG(info,  (value_t) (clock() - begin_time) / CLOCKS_PER_SEC);
+	c1 = Tensor::random(dim1);
+	c1 = Tensor::random(dim2);
+	begin_time = clock();
+	tmp(ii,kk,ll,mm^2) = ones(nn)* c1(nn,ii,jj,kk)*c2(ll,jj,mm^2);
+	XERUS_LOG(info,  (value_t) (clock() - begin_time) / CLOCKS_PER_SEC);
 
 	return 0;
 }
