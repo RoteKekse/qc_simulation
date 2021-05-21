c4b15e2471b62f75a6ed56a137c4da7b811d361b
Micha <goette@math.tu-berlin.de>
2021-05-21 08:41:05 +0200

    update

--- code/TestContract.cpp
+++ code/TestContract.cpp
@@ -8,7 +8,7 @@ using xerus::misc::operator<<;
 
 
 int main(){
-	Index ii,jj,kk,ll,mm;
+	Index ii,jj,kk,ll,mm,nn;
 	TTOperator H;
 	Tensor tmp;
 	std::string name = "data/n2_1.05_cc-pvtz_H.ttoperator";
@@ -18,6 +18,7 @@ int main(){
 	auto c2 = H.get_component(61);
 	auto dim1 = c1.dimensions;
 	auto dim2 = c2.dimensions;
+	auto ones = Tensor::ones({dim1[2]});
 	XERUS_LOG(info,c1.dimensions);
 	XERUS_LOG(info,c2.dimensions);
 
@@ -39,6 +40,7 @@ int main(){
 
 	c1 = H.get_component(55);
 	c2 = H.get_component(56);
+	 ones = Tensor::ones({dim1[2]});
 	dim1 = c1.dimensions;
 	dim2 = c2.dimensions;
 	XERUS_LOG(info,c1.dimensions);
@@ -46,19 +48,19 @@ int main(){
 
 	begin_time = clock();
 	tmp(ii^2,kk,ll,mm^2) = c1(ii^2,jj,kk)*c2(ll,jj,mm^2);
-	XERUS_LOG(info,  (value_t) (clock() - begin_time) / CLOCKS_PER_SEC);
+		XERUS_LOG(info,  (value_t) (clock() - begin_time) / CLOCKS_PER_SEC);
 
-	c1.use_dense_representation();
-	c2.use_dense_representation();
-	begin_time = clock();
-	tmp(ii^2,kk,ll,mm^2) = c1(ii^2,jj,kk)*c2(ll,jj,mm^2);
-	XERUS_LOG(info,  (value_t) (clock() - begin_time) / CLOCKS_PER_SEC);
+		c1.use_dense_representation();
+		c2.use_dense_representation();
+		begin_time = clock();
+		tmp(ii^2,kk,ll,mm^2) = c1(ii^2,jj,kk)*c2(ll,jj,mm^2);
+		XERUS_LOG(info,  (value_t) (clock() - begin_time) / CLOCKS_PER_SEC);
 
-	c1 = Tensor::random(dim1);
-	c1 = Tensor::random(dim2);
-	begin_time = clock();
-	tmp(ii^2,kk,ll,mm^2) = c1(ii^2,jj,kk)*c2(ll,jj,mm^2);
-	XERUS_LOG(info,  (value_t) (clock() - begin_time) / CLOCKS_PER_SEC);
+		c1 = Tensor::random(dim1);
+		c1 = Tensor::random(dim2);
+		begin_time = clock();
+		tmp(ii^2,kk,ll,mm^2) = c1(ii^2,jj,kk)*c2(ll,jj,mm^2);
+		XERUS_LOG(info,  (value_t) (clock() - begin_time) / CLOCKS_PER_SEC);
 
 	return 0;
 }
