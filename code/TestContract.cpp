a0f1f4206aeff8d734ff43a70c32bea7b1117b9d
Micha <goette@math.tu-berlin.de>
2021-05-21 09:15:26 +0200

    update

--- code/TestContract.cpp
+++ code/TestContract.cpp
@@ -23,19 +23,22 @@ int main(){
 	XERUS_LOG(info,c2.dimensions);
 
 	clock_t begin_time = clock();
-	tmp(ii,kk,ll,mm^2) = ones(nn)* c1(nn,ii,jj,kk)*c2(ll,jj,mm^2);
+	tmp(ii,jj,kk) = ones(nn)* c1(nn,ii,jj,kk);
+	tmp(ii,kk,ll,mm^2) = tmp(ii,jj,kk)*c2(kk,ll,mm^2);
 	XERUS_LOG(info,  (value_t) (clock() - begin_time) / CLOCKS_PER_SEC);
 
 	c1.use_dense_representation();
 	c2.use_dense_representation();
 	begin_time = clock();
-	tmp(ii,kk,ll,mm^2) = ones(nn)* c1(nn,ii,jj,kk)*c2(ll,jj,mm^2);
+	tmp(ii,jj,kk) = ones(nn)* c1(nn,ii,jj,kk);
+	tmp(ii,kk,ll,mm^2) = tmp(ii,jj,kk)*c2(kk,ll,mm^2);
 	XERUS_LOG(info,  (value_t) (clock() - begin_time) / CLOCKS_PER_SEC);
 
 	c1 = Tensor::random(dim1);
 	c1 = Tensor::random(dim2);
 	begin_time = clock();
-	tmp(ii,kk,ll,mm^2) = ones(nn)* c1(nn,ii,jj,kk)*c2(ll,jj,mm^2);
+	tmp(ii,jj,kk) = ones(nn)* c1(nn,ii,jj,kk);
+	tmp(ii,kk,ll,mm^2) = tmp(ii,jj,kk)*c2(kk,ll,mm^2);
 	XERUS_LOG(info,  (value_t) (clock() - begin_time) / CLOCKS_PER_SEC);
 
 	c1 = H.get_component(55);
@@ -47,19 +50,22 @@ int main(){
 	XERUS_LOG(info,c2.dimensions);
 
 	begin_time = clock();
-	tmp(ii,kk,ll,mm^2) = ones(nn)* c1(nn,ii,jj,kk)*c2(ll,jj,mm^2);
+	tmp(ii,jj,kk) = ones(nn)* c1(nn,ii,jj,kk);
+	tmp(ii,kk,ll,mm^2) = tmp(ii,jj,kk)*c2(kk,ll,mm^2);
 	XERUS_LOG(info,  (value_t) (clock() - begin_time) / CLOCKS_PER_SEC);
 
 	c1.use_dense_representation();
 	c2.use_dense_representation();
 	begin_time = clock();
-	tmp(ii,kk,ll,mm^2) = ones(nn)* c1(nn,ii,jj,kk)*c2(ll,jj,mm^2);
+	tmp(ii,jj,kk) = ones(nn)* c1(nn,ii,jj,kk);
+	tmp(ii,kk,ll,mm^2) = tmp(ii,jj,kk)*c2(kk,ll,mm^2);
 	XERUS_LOG(info,  (value_t) (clock() - begin_time) / CLOCKS_PER_SEC);
 
 	c1 = Tensor::random(dim1);
 	c1 = Tensor::random(dim2);
 	begin_time = clock();
-	tmp(ii,kk,ll,mm^2) = ones(nn)* c1(nn,ii,jj,kk)*c2(ll,jj,mm^2);
+	tmp(ii,jj,kk) = ones(nn)* c1(nn,ii,jj,kk);
+	tmp(ii,kk,ll,mm^2) = tmp(ii,jj,kk)*c2(kk,ll,mm^2);
 	XERUS_LOG(info,  (value_t) (clock() - begin_time) / CLOCKS_PER_SEC);
 
 	return 0;
