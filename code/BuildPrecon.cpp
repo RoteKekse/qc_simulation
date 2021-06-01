196ad87505ba0fe1ac6ee803ebcec90a4d330d84
Micha <goette@math.tu-berlin.de>
2021-06-01 15:43:25 +0200

    update

--- code/BuildPrecon.cpp
+++ code/BuildPrecon.cpp
@@ -58,6 +58,7 @@ int main(int argc, char* argv[]) {
 	Tensor nuc;
 	name = "data/"+static_cast<std::string>(geom)+"_"+static_cast<std::string>(basisname)+"_nuc.tensor";
 	read_from_disc(name,nuc);
+	XERUS_LOG(info, "shift = " << shift);
 	shift += nuc[0];
 	XERUS_LOG(info, "shift = " << shift);
 	std::vector<value_t> HFev;
